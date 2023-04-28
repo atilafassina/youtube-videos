a22d406a6533b81d250f655223e00efa178fcf47
Atila Fassina <atila@fassina.eu>
2023-04-28 18:30:02 +0200

    remove max-age

--- lib/config.ts
+++ lib/config.ts
@@ -1,4 +1,4 @@
 const DAY_IN_SECONDS = 60 * 60 * 24
-export const CACHE_CONTROL_HEADER = `s-maxage=${DAY_IN_SECONDS}, stale-while-revalidate=${
+export const CACHE_CONTROL_HEADER = `s-maxage=1, stale-while-revalidate=${
   3 * DAY_IN_SECONDS
 }`
